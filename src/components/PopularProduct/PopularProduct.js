17c5651c3d77fafb0adc056b1398c3ad6cbbbee6
Goq-96 <abrahamyan199605@inbox.ru>
2023-04-01 19:45:31 +0400

    Changed PopularProduct component

--- src/components/PopularProduct/PopularProduct.js
+++ src/components/PopularProduct/PopularProduct.js
@@ -1,10 +1,84 @@
 import './_popularProduct.scss';
+import DB from './../../DB/DB.json'
 
 function PopularProduct() {
+
+  const isBasket = () => {
+  }
+
+
+  const img1 = DB.popular[0].url;
+  const img2 = DB.popular[1].url;
+  const img3 = DB.popular[2].url;
+  
   return (
     <>
       <div className="popularProduct">
-        Популярные товары 5555555555
+        <div className="popularProduct_contener">
+          <div className="popular_contner_text">
+            <p className="popular_text">
+              Популярные товары
+            </p>
+          </div>
+
+          <div className="store_contener">
+            <div className="block_all">
+
+              <div className="block_1 block">
+                <div className="imgBlock">
+                  <div className='imgPrice'>
+                    <p>Осталось 3 шт.</p>
+                    <p>Скидка 10%</p>
+                  </div>
+                  <img src={img1} alt="img-1 " />
+                </div>
+                <div className="title">
+                  <p>{DB.popular[0].title}</p>
+                  <span>{DB.popular[0].price}</span>
+                </div>
+                <div className="basket">
+                  <button onClick={isBasket}>В корзину</button>
+                </div>
+              </div>
+
+              <div className="block_2 block">
+                <div className="imgBlock">
+                  <div className='imgPrice'>
+                    <p>Бесплатная доставка</p>
+                  </div>
+                  <img src={img2} alt="img-1 " />
+                </div>
+                <div className="title">
+                  <p>{DB.popular[1].title}</p>
+                  <span>{DB.popular[1].price}</span>
+                </div>
+                <div className="basket">
+                  <button onClick={isBasket}>В корзину</button>
+                </div>
+                <div className="title"></div>
+                <div className="basket"></div>
+              </div>
+
+              <div className="block_3 block">
+                <div className="imgBlock">
+                  <div className='imgPrice'>
+                    <p>Скидка 10%</p>
+                  </div>
+                  <img src={img3} alt="img-1 " />
+                </div>
+                <div className="title">
+                  <p>{DB.popular[2].title}</p>
+                  <span>{DB.popular[2].price}</span>
+                </div>
+                <div className="basket">
+                  <button onClick={isBasket}>В корзину</button>
+                </div>
+                <div className="title"></div>
+                <div className="basket"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
